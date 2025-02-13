b6b48250b98fb7de192110707c8c6d35cdd2406d
SivasankarZ <sivasankar.kumaravel@zalora.com>
2025-02-13 13:45:16 +0530

    Initial Commit

---
+++ TravelNanban/TravelNanbanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelNanbanApp.swift
+//  TravelNanban
+//
+//  Created by Sivasankar on 13/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelNanbanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelNanbanTests/TravelNanbanTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelNanbanTests.swift
+//  TravelNanbanTests
+//
+//  Created by Sivasankar on 13/2/25.
+//
+
+import Testing
+@testable import TravelNanban
+
+struct TravelNanbanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
